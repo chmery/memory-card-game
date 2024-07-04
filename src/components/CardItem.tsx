e5761c4477218915fe40d9f7dbcdedece3abd08f
Wiktor Filip <karektv@gmail.com>
2024-07-04 17:25:44 +0200

    board responsiveness

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -7,15 +7,16 @@ interface Props {
 
 export const CardItem = ({ card, onFlip }: Props) => {
     const canFlip = !card.isGuessed && !card.isFlipped;
+    const isValueVisible = card.isFlipped || card.isGuessed;
 
     return (
         <div
             onClick={() => canFlip && onFlip(card.id)}
-            className={`bg-primary600 text-6xl aspect-square rounded-lg flex items-center justify-center ${
-                !card.isFlipped && !card.isGuessed && "font-black text-primary300 cursor-pointer"
+            className={`bg-primary600 relative text-3xl sm:text-6xl  aspect-square rounded-lg flex items-center justify-center ${
+                canFlip && "font-black text-primary300 cursor-pointer"
             }`}
         >
-            {card.isFlipped || card.isGuessed ? card.value : "?"}
+            <span className="absolute">{isValueVisible ? card.value : "?"}</span>
         </div>
     );
 };
