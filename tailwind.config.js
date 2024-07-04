77b3bb5c87aee95e6ab445e85535a2ad4038b1e8
Wiktor Filip <karektv@gmail.com>
2024-07-04 09:54:02 +0200

    css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        colors: {
+            secondary700: "#1fb6ff",
+            secondary500: "#7e5bef",
+            secondary400: "#ff49db",
+            secondary200: "#777789",
+            primary300: "#9898D9",
+            primary600: "#5C5C9A",
+        },
         extend: {},
     },
     plugins: [],
