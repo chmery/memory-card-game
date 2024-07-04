7551fc7a115cac2af0aeeeaedb33e1acd9b24375
Wiktor Filip <karektv@gmail.com>
2024-07-04 11:34:25 +0200

    card array generation

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,36 @@ interface Card {
 const CARD_VALUES = ["😇", "👾", "🤖", "🤠", "👻", "😸", "🧠", "🧤", "🦴", "🥳", "👽", "💍"];
 
 function App() {
+    const getCardObjects = () => {
+        return CARD_VALUES.map((value) => {
+            return {
+                id: crypto.randomUUID(),
+                value,
+                isFlipped: false,
+                isGuessed: false,
+            };
+        });
+    };
+
+    const getDuplicatedCards = (cards: Card[]) => {
+        const duplicated = [];
+        for (const card of cards) {
+            duplicated.push(card, card);
+        }
+        return duplicated;
+    };
+
+    const getShuffledCards = (cards: Card[]) => {
+        const cardsCopy = structuredClone(cards);
+
+        for (let i = cardsCopy.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [cardsCopy[i], cardsCopy[j]] = [cardsCopy[j], cardsCopy[i]];
+        }
+
+        return cardsCopy;
+    };
+
     return (
         <main className="max-w-[50rem] m-auto mt-4 px-2">
             <h1 className="text-3xl italic font-black text-center mb-4">MEMORY CARD GAME</h1>
