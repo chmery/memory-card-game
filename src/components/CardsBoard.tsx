ed4875bd59253647e5098c23aee2177fe9883f1d
Wiktor Filip <karektv@gmail.com>
2024-07-04 22:00:25 +0200

    responsiveness improvements

--- src/components/CardsBoard.tsx
+++ src/components/CardsBoard.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const CardsBoard = ({ cards, onFlip }: Props) => {
     return (
-        <div className="bg-secondary700 rounded-xl mb-4 p-4 grid-rows-4 grid grid-flow-col gap-3">
+        <div className="bg-secondary700 rounded-xl mb-4 p-3 sm:p-4 grid-rows-4 grid grid-flow-col gap-2 sm:gap-3">
             {cards.map((card, i) => (
                 <CardItem key={i} card={card} onFlip={onFlip} />
             ))}
--- src/components/UpperPanel.tsx
+++ src/components/UpperPanel.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export const UpperPanel = ({ onReset, movesCount, time }: Props) => {
     return (
-        <div className="bg-secondary700 rounded-xl mb-4 flex gap-4 justify-between p-4">
+        <div className="bg-secondary700 rounded-xl mb-4 flex gap-3 sm:gap-4 justify-between p-3 sm:p-4 flex-wrap text-nowrap">
             <div className="p-2 bg-secondary500 px-5 rounded-lg flex-1 text-center font-semibold">
                 Time: <span className="font-bold">{getFormattedTime(time)}</span>
             </div>
@@ -18,7 +18,7 @@ export const UpperPanel = ({ onReset, movesCount, time }: Props) => {
             </div>
             <button
                 onClick={onReset}
-                className="bg-secondary400 font-semibold gap-2 flex-1 flex justify-center items-center rounded-lg hover:bg-secondary200 transition px-5"
+                className="bg-secondary400 font-semibold gap-2 p-2 flex-1 flex justify-center items-center rounded-lg hover:bg-secondary200 transition px-5"
             >
                 <ResetArrow className="size-6" /> Restart
             </button>
